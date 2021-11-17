6ba5013404ddeccbf0a841a3727b53570641ef45
Артём <cryneso@gmail.com>
2021-11-17 04:33:46 -0800

    формы шаг 6

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,6 +2,8 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
+from MainApp.forms import SnippetForm
+
 
 
 def index_page(request):
@@ -10,8 +12,19 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    if request.method == "GET":
+        form = SnippetForm()
+        context = {
+            'pagename': 'Добавление нового сниппета',
+            'form': form
+        }
+        return render(request, 'pages/add_snippet.html', context)
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list_snippet")
+        return render(request, 'add_snippet.html', {'form': form})
 
 
 def snippets_page(request):
