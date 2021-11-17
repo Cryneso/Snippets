cca6c5e6641d7bd2835e5c8e21490474aae1e37c
Артём <cryneso@gmail.com>
2021-11-17 00:52:16 -0800

    module 5 front

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from MainApp import views
 
 urlpatterns = [
-    path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
+    path('', views.index_page, name='home'),
+    path('snippets/add', views.add_snippet_page, name='add_snippet'),
+    path('snippets/list', views.snippets_page, name='list_snippet'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
