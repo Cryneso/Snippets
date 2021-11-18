a9003b4e9860db7a6e2ef69fb135ece915de3311
Артём <cryneso@gmail.com>
2021-11-18 07:03:57 -0800

    test 2

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -11,4 +11,3 @@ urlpatterns = [
     path('snippets/edit/<int:id>', views.edit_snippet, name='edit_snippet'),
     path('snippets/delete/<int:id>', views.delete_snippet, name='delete_snippet')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
