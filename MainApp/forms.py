4c51cb899f5125aff1c525b3150c64daf70cb808
Артём <cryneso@gmail.com>
2021-12-08 06:42:05 -0800

    format

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -3,7 +3,7 @@ from MainApp.models import Snippet
 
 
 class SnippetForm(ModelForm):
-   class Meta:
-       model = Snippet
-       # Описываем поля, которые будем заполнять в форме
-       fields = ['name', 'lang', 'code']
+    class Meta:
+        model = Snippet
+        # Описываем поля, которые будем заполнять в форме
+        fields = ['name', 'lang', 'code']
