6f26ef071b865622f2f52a50217e174bffe2a9a2
Артём <cryneso@gmail.com>
2021-12-09 00:13:24 -0800

    opimize

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -2,18 +2,27 @@ from django.forms import ModelForm
 from django.forms.widgets import Select, TextInput, Textarea
 from MainApp.models import Snippet
 
+LANGS = (
+    ('python', 'Python'),
+    ('java', 'Java'),
+    ('php', 'PHP'),
+    ('cs', 'C#'),
+)
 
 class SnippetForm(ModelForm):
     class Meta:
         model = Snippet
         # Описываем поля, которые будем заполнять в форме
         fields = ['name', 'lang', 'code']
+        
         widgets = {
             'name': TextInput(attrs={"class": "form-control form-control-lg", 'placeholder': 'Название сниппета'}),
-            'lang': Select(attrs={"class": "form-control form-control-lg"}, choices=[('python', 'Python'), ('java', 'Java'), ('php', 'PHP')]),
+            'lang': Select(attrs={"class": "form-control form-control-lg"}, choices=LANGS),
             'code': Textarea(attrs={"class": "form-control form-control-lg"}),
         }
+        
         labels = {
             'name': 'Название',
-            'lang': 'Язык'
+            'lang': 'Язык',
+            'code': 'Код',
         }
