d7113b8f4c6fbd121bea62ed17bfbbfa5870df35
Артём <cryneso@gmail.com>
2021-12-09 02:12:03 -0800

    авторизация

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Snippet(models.Model):
     name = models.CharField(max_length=100)
     lang = models.CharField(max_length=30)
     code = models.TextField(max_length=5000)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- MainApp/views.py
+++ MainApp/views.py
@@ -3,11 +3,32 @@ from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
 from MainApp.forms import SnippetForm
+from django.contrib import auth
 
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
     return render(request, 'pages/index.html', context)
+
+
+def login_page(request):
+   if request.method == 'POST':
+       username = request.POST.get("username")
+       password = request.POST.get("password")
+       # print("username =", username)
+       # print("password =", password)
+       user = auth.authenticate(request, username=username, password=password)
+       if user is not None:
+           auth.login(request, user)
+       else:
+           # Return error message
+           pass
+   return redirect('home')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
+
 
 
 def add_snippet_page(request):
@@ -22,7 +43,10 @@ def add_snippet_page(request):
     if request.method == "POST":
         form = SnippetForm(request.POST)
         if form.is_valid():
-            form.save()
+            snippet = form.save(commit=False)
+            if request.user.is_authenticated:
+                snippet.user = request.user
+                snippet.save()
             return redirect("list_snippet")
         return render(request, 'add_snippet.html', {'form': form})
 
